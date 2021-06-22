b1346a0b147ab3fa165b7e86510b90a27019377a
Rouyu Chen <u6539353@anu.edu.au>
2021-06-22 17:07:17 +1000

    Make sidebar underneath the headbar, make it fixed on the left side, responsive effect is good.

---
+++ financial_schedule/frontend/src/components/Sidebar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <b-sidebar id="sidebar-no-header" z-index=-1
+         aria-labelledby="sidebar-no-header-title" width="150px" no-close-on-route-change visible>
+      <template>
+        <div class="p-3">
+          <nav class="mb-3">
+            <b-nav vertical>
+              <b-nav-item active @click="view_all">View All</b-nav-item>
+              <b-nav-item href="#link-1" @click="view_bills">Bills</b-nav-item>
+              <b-nav-item href="#" @click="view_invoices">Invoices</b-nav-item>
+              <b-nav-item href="#" @click="view_quote">Quote</b-nav-item>
+              <b-nav-item href="#" @click="view_porder">Purchase Order</b-nav-item>
+              <!-- <b-button v-b-toggle.sidebar-no-header>Toggle Sidebar</b-button> -->
+            </b-nav>
+          </nav>
+        </div>
+      </template>
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        view_all() {
+            console.log("view all");
+            return ""
+        },
+        view_bills() {
+            console.log("view bills");
+        },
+        view_invoices() {
+            console.log("view invoices");
+        },
+        view_quote() {  
+            console.log("view quote");
+        },
+        view_porder() {
+            console.log("view purchase order");
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- financial_schedule/frontend/src/views/Home.vue
+++ financial_schedule/frontend/src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
     <Navbar/>
+    <Sidebar/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbar from "@/components/Navbar.vue";
+import Sidebar from "@/components/Sidebar.vue"
 
 export default {
   name: "Home",
   components: {
-    Navbar,
+    Navbar, Sidebar
   },
 };
 </script>
